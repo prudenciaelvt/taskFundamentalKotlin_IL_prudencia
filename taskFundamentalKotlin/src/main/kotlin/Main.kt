2cc1acc76b2c2108ed7cf6272c1bdab0372877d3
prudenciaelvt <145270164+prudenciaelvt@users.noreply.github.com>
2024-10-29 20:27:24 +0700

    first commit

---
+++ taskFundamentalKotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,209 @@
+package org.example
+
+
+////TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+//// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+
+// Variable
+//fun main() {
+//    var nama: String = "Prudencia Belva Cynara Trana Putri"
+//    nama = "Rania Siana"
+//    println(nama)
+//}
+
+//Data types - Char
+//fun main() {
+//    var abjad = 'H'
+//
+//    println("Huruf " + abjad++)
+//    println("Huruf " + abjad++)
+//    println("Huruf " + abjad++)
+//    println("Huruf " + abjad--)
+//    println("Huruf " + abjad--)
+//    println("Huruf " + abjad--)
+//}
+
+//Data Types - String
+//fun main () {
+//    val namaPanjang = "Prudencia Belva Cynara Trana Putri"
+//    val karakterPertama = namaPanjang[0]
+//
+//    println("Karakter pertama dari nama panjang itu adalah $karakterPertama")
+//}
+
+//Data Types - String
+//fun main() {
+//    val biodata = "Prudencia Belva Cynara Trana Putri \nby \"Android Mobile Development\""
+//
+//    println(biodata)
+//}
+
+//Data Types - String
+//fun main() {
+//    val pernyataan = """
+//        Prudencia Belva Cynara Trana Putri
+//        ----------------------------------
+//        Android Mobile Development & UIUX
+//        Di Infinite Learning
+//        """.trimIndent()
+//
+//    println(pernyataan)
+//}
+
+//Data Types - Boolean
+//fun main() {
+//    val A = true
+//    val B = false
+//
+//    println("A && B = " + (A && B))
+//    println("A || B = " + (A || B))
+//    println("!B = " + (!B))
+//
+//}
+
+//Data Types - Boolean Expression
+//fun main() {
+//    val a: Int = 75
+//    val b: Int = 30
+//
+//    println("a > b = " + (a > b))
+//    println("a < b = " + (a < b))
+//    println("a >= b = " + (a >= b))
+//    println("a <= b = " + (a <= b))
+//    println("a == b = " + (a == b))
+//    println("a != b = " + (a != b))
+//}
+
+//Numbers
+//fun main() {
+//    val x: Int = 50000
+//    val y: Double = 250.50
+//    val z: Float = 200.25f
+//    val w: Long = 5000000000L
+//    val t: Short = 15
+//    val u: Byte = Byte.MIN_VALUE
+//
+//    println("Int value is " + x)
+//    println("Double value is " + y)
+//    println("Float value is " + z)
+//    println("Long value is " + w)
+//    println("Short value is " + t)
+//    println("Byte value is " + u)
+//}
+
+//Get and Set the element of an Array
+//fun main() {
+//    val tim = arrayOf<String>("Tim A", "Tim B", "Tim C", "Tim D")
+//
+//    println(tim[0])
+//
+//    println(tim.get(1))
+//
+//    tim[0] = "Tim Alpha"
+//    tim.set(1, "Tim Beta")
+//
+//    println(tim[0])
+//    println(tim.get(1))
+//    println(tim.get(2))
+//}
+
+//Primitive type Arrays
+//fun main() {
+//    val intAngka = intArrayOf(2,3,4,5)
+//    intAngka[3] = 40
+//
+//    println(intAngka[3])
+//
+//}
+
+//With String template
+//fun main() {
+//    val nama = "Prudencia Belva Cynara Trana Putri"
+//    println("Nama saya adalah $nama")
+//}
+
+//Without String Template
+//fun main() {
+//    val nama = "Prudencia Belva Cynara Trana Putri"
+//    println("Nama saya adalah " + nama)
+//}
+
+//String Template
+//fun main() {
+//    val poin = 74
+//    print("Poin ${if (poin >= 75) "Anda Lolos" else "Anda tidak lolos"}")
+//}
+
+//Save Calls and Elvis Operator
+//fun main() {
+//    var waktu: String? = null
+//    var kondisi = "Booked"
+//
+//    kondisi = "Checkout"
+//
+//    if (kondisi.equals("Checkout")) {
+//        waktu = "1/3/2022 19:30:00"
+//    }
+//
+//    print(waktu)
+//}
+
+//Function
+//fun identity() {
+//    println("Nama Prudencdia Belva Cynara Trana Putri")
+//    println("Android Mobile Development & UIUX Design")
+//}
+//
+//fun main() {
+//    identity()
+//}
+
+//Function Parameter
+//fun dataNama(namaAwal: String, namaAkhir: String) {
+//    println("Hello, $namaAwal $namaAkhir")
+//}
+//
+//fun main() {
+//    dataNama("Prudencia", "Putri")
+//}
+
+//Function Default Parameter
+//fun dataNama(namaAwal: String, namaAkhir: String = "" ) {
+//    println("Hello, $namaAwal $namaAkhir")
+//}
+//
+//fun main() {
+//    dataNama("Prudencia")
+//    dataNama("Prudencia", "Putri")
+//}
+
+
+//Function Named Argument
+//fun identity(
+//    namaAwal: String,
+//    namaTengah: String,
+//    namaAkhir: String
+//) {
+//    println("Hallo $namaAwal $namaTengah $namaAkhir")
+//}
+//
+//fun main () {
+//    identity(
+//        namaAwal = "Prudencia",
+//        namaTengah = "Cynara",
+//        namaAkhir = "Putri"
+//    )
+//}
+
+
+
+
+
+
+
+
+
+
+
+
